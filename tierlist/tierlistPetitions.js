a05ef556a82b80cfa184c7e4989a5acd36697d56
TomasRosello <tomas.xeresa@gmail.com>
2026-01-29 12:06:59 +0100

    Tierlist

---
+++ tierlist/tierlistPetitions.js
@@ -0,0 +1,14 @@
+export const armocheG1Main = "https://uwuowo.mathi.moe/api/stats/raids?boss=Armoche%2C+Sentinel+of+the+Abyss&difficulty=Hard&patch=jan26&filterBy=ilvl&type=bdps&support=true&minIlvl=1730&maxIlvl=1750";
+export const armocheG1Alt = "https://uwuowo.mathi.moe/api/stats/raids?boss=Armoche%2C+Sentinel+of+the+Abyss&difficulty=Normal&patch=jan26&filterBy=ilvl&type=bdps&support=true&minIlvl=1710&maxIlvl=1730";
+export const armocheG2Main = "https://uwuowo.mathi.moe/api/stats/raids?boss=Armoche%2C+Sentinel+of+the+Abyss&difficulty=Hard&patch=jan26&filterBy=ilvl&type=bdps&support=true&minIlvl=1730&maxIlvl=1750";
+export const armocheG2Alt = "https://uwuowo.mathi.moe/api/stats/raids?boss=Armoche%2C+Sentinel+of+the+Abyss&difficulty=Normal&patch=jan26&filterBy=ilvl&type=bdps&support=true&minIlvl=1710&maxIlvl=1730";
+export const mordumG1Main = "https://uwuowo.mathi.moe/api/stats/raids?boss=Infernas&difficulty=Hard&patch=jan26&filterBy=ilvl&type=bdps&support=true&minIlvl=1730&maxIlvl=1750";
+export const mordumG1Alt = "https://uwuowo.mathi.moe/api/stats/raids?boss=Infernas&difficulty=Hard&patch=jan26&filterBy=ilvl&type=bdps&support=true&minIlvl=1710&maxIlvl=1730";
+export const mordumG2Main = "https://uwuowo.mathi.moe/api/stats/raids?boss=Blossoming+Fear%2C+Naitreya&difficulty=Hard&patch=jan26&filterBy=ilvl&type=bdps&support=true&minIlvl=1730&maxIlvl=1750";
+export const mordumG2Alt = "https://uwuowo.mathi.moe/api/stats/raids?boss=Blossoming+Fear%2C+Naitreya&difficulty=Hard&patch=jan26&filterBy=ilvl&type=bdps&support=true&minIlvl=1710&maxIlvl=1730";
+export const mordumG3Main = "https://uwuowo.mathi.moe/api/stats/raids?boss=Flash+of+Punishment&difficulty=Hard&patch=jan26&filterBy=ilvl&type=bdps&support=true&minIlvl=1730&maxIlvl=1750";
+export const mordumG3Alt = "https://uwuowo.mathi.moe/api/stats/raids?boss=Flash+of+Punishment&difficulty=Hard&patch=jan26&filterBy=ilvl&type=bdps&support=true&minIlvl=1710&maxIlvl=1730";
+export const kazerosG1Main = "https://uwuowo.mathi.moe/api/stats/raids?boss=Abyss+Lord+Kazeros&difficulty=Hard&patch=jan26&filterBy=ilvl&type=bdps&support=true&minIlvl=1730&maxIlvl=1750";
+export const kazerosG1Alt = "https://uwuowo.mathi.moe/api/stats/raids?boss=Abyss+Lord+Kazeros&difficulty=Normal&patch=jan26&filterBy=ilvl&type=bdps&support=true&minIlvl=1710&maxIlvl=1730";
+export const kazerosG2Main = "https://uwuowo.mathi.moe/api/stats/raids?boss=Death+Incarnate+Kazeros&difficulty=Hard&patch=jan26&filterBy=ilvl&type=udps&support=false&minIlvl=1730&maxIlvl=1750";
+export const kazerosG2Alt = "https://uwuowo.mathi.moe/api/stats/raids?boss=Death+Incarnate+Kazeros&difficulty=Normal&patch=jan26&filterBy=ilvl&type=udps&support=false&minIlvl=1710&maxIlvl=1730";
