0ea9fe1eeaad5576870594afb0a985f2e4705a06
TomasRosello <tomas.xeresa@gmail.com>
2026-01-30 13:13:56 +0100

    Alt dynamic fetch

--- tierlist/tierlistPetitions.js
+++ tierlist/tierlistPetitions.js
@@ -12,3 +12,22 @@ export const kazerosG1Main = "https://uwuowo.mathi.moe/api/stats/raids?boss=Abys
 export const kazerosG1Alt = "https://uwuowo.mathi.moe/api/stats/raids?boss=Abyss+Lord+Kazeros&difficulty=Normal&patch=jan26&filterBy=ilvl&type=bdps&support=true&minIlvl=1710&maxIlvl=1730";
 export const kazerosG2Main = "https://uwuowo.mathi.moe/api/stats/raids?boss=Death+Incarnate+Kazeros&difficulty=Hard&patch=jan26&filterBy=ilvl&type=udps&support=false&minIlvl=1730&maxIlvl=1750";
 export const kazerosG2Alt = "https://uwuowo.mathi.moe/api/stats/raids?boss=Death+Incarnate+Kazeros&difficulty=Normal&patch=jan26&filterBy=ilvl&type=udps&support=false&minIlvl=1710&maxIlvl=1730";
+
+export const RAID_URLS = {
+  armocheG1Main: armocheG1Main,
+  armocheG1Alt: armocheG1Alt,
+  armocheG2Main: armocheG2Main,
+  armocheG2Alt: armocheG2Alt,
+
+  mordumG1Main: mordumG1Main,
+  mordumG1Alt: mordumG1Alt,
+  mordumG2Main: mordumG2Main,
+  mordumG2Alt: mordumG2Alt,
+  mordumG3Main: mordumG3Main,
+  mordumG3Alt: mordumG3Alt,
+
+  kazerosG1Main: kazerosG1Main,
+  kazerosG1Alt: kazerosG1Alt,
+  kazerosG2Main: kazerosG2Main,
+  kazerosG2Alt: kazerosG2Alt,
+};
