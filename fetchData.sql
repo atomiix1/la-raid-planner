b7a7c8a7871d67a90b0b8f2dad4a7e33b538c4fa
TomasRosello <tomas.xeresa@gmail.com>
2026-03-27 11:37:04 +0100

    Nueva query

--- fetchData.sql
+++ fetchData.sql
@@ -1,1 +1,24 @@
-SELECT entity.name, entity.dps, entity.unbuffed_dps, encounter_preview.fight_start, encounter_preview.current_boss, encounter_preview.difficulty FROM entity JOIN encounter_preview ON entity.encounter_id = encounter_preview.id WHERE entity.entity_type = 'PLAYER';
+SELECT 
+    entity.name, 
+    entity.dps, 
+    entity.unbuffed_dps, 
+    encounter_preview.fight_start, 
+    encounter_preview.current_boss, 
+    encounter_preview.difficulty 
+FROM entity 
+JOIN encounter_preview 
+    ON entity.encounter_id = encounter_preview.id 
+WHERE 
+    entity.entity_type = 'PLAYER'
+    AND encounter_preview.difficulty IS NOT NULL
+    AND encounter_preview.difficulty <> ''
+    AND entity.name IN (
+        'Naromiix', 'Satoriix', 'Atomiix', 'Katoriix', 'Tomoriix', 'Feraliix',
+        'Aedhe', 'Boreahl', 'Nawinar', 'Lekhanar', 'Knaath', 'Dynwen', 'Kadell',
+        'Brunildâ', 'Lirèsa', 'Yudellia', 'Eskkol', 'Âvlora', 'Vriytra',
+        'Kijyodouji', 'Luminarys', 'Katsushiika', 'Hiymeko', 'Talaxia',
+        'Neïzha', 'Dhaniria', 'Danîkha', 'Aësson', 'Songjoah', 'Zukka',
+        'Alavhel', 'Eirÿs', 'Eÿsa', 'Xÿrris', 'Kiersah', 'Sebadi',
+        'Liduvina', 'Simzhae', 'Chalinda', 'Uriangera', 'Crisostoma',
+        'Orkhanx', 'Orkhanar', 'Orkhanavalk', 'Örkhana', 'Orkhans', 'Orkhan'
+    );
